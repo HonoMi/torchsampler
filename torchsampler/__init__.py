fb173eda448b66334c92172ef7fe0dc9ce68d327
honoka <hello@hello_world.com>
2020-11-10 16:46:00 +0900

    [update]

--- torchsampler/__init__.py
+++ torchsampler/__init__.py
@@ -1,2 +1,2 @@
-from .imbalanced_sampler import WeightedDatsetSampler, DatasetInterface, build_class_balanced_sampler
+from .imbalanced_sampler import WeightedDatasetSampler, DatasetInterface, build_class_balanced_sampler
 from .dataloader import DataLoader
