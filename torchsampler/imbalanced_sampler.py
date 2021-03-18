60f696e52097d04939c21848fe0d2fd9f9e7155c
honoka <hello@hello_world.com>
2021-03-18 14:42:26 +0900

    [update]

--- torchsampler/imbalanced_sampler.py
+++ torchsampler/imbalanced_sampler.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from torch.utils.data import Dataset as PytorchDatset
 
@@ -14,7 +16,7 @@ class DatasetInterface(PytorchDatset):
 def build_class_balanced_sampler(dataset,
                                  get_label_func,
                                  indices=None,
-                                 size=None):
+                                 **kwargs):
 
     indices = indices or list(range(len(dataset)))
     label_to_count = {}
@@ -30,31 +32,71 @@ def build_class_balanced_sampler(dataset,
                for idx in indices]
     weights = torch.DoubleTensor(weights)
 
-    return WeightedDatasetSampler(dataset,
-                                  weights,
-                                  indices=indices,
-                                  size=size)
+    return WeightedDatasetSampler(len(dataset),
+                                  sample_weights=weights,
+                                  dataset_indices=indices,
+                                  **kwargs)
 
 
 class WeightedDatasetSampler(torch.utils.data.sampler.Sampler):
+    """WeightedDatasetSampler
 
+    sample_weights=None で呼んだ場合，サンプリングは行われず，単にデータセット中のサンプル全てが使われる．
+    一様分布からサンプリングされる，という訳ではないことに注意．
+    """
+
+    # TODO: fix_seed => fix_seed_for_weighted_sampler
     def __init__(self,
-                 dataset: DatasetInterface,
-                 weights,
-                 indices=None,
-                 size=None):
+                 # dataset: DatasetInterface,
+                 dataset_size: int,
+                 sample_weights: Optional[torch.Tensor],
+                 same_samples_over_epochs=False,
+                 sample_then_shuffle_every_epoch=False,
+                 dataset_indices=None):
 
-        self.indices = indices or list(range(len(dataset)))
-        self.size = size or len(self.indices)
-        if len(weights) != self.size:
-            raise ValueError('len(weights) != datase size')
-        self.weights = torch.tensor(weights)
+        # self._dataset_indices = dataset_indices or list(range(len(dataset)))
+        self._size = dataset_size
+        self._dataset_indices = dataset_indices or torch.tensor(list(range(dataset_size)), dtype=torch.int64)
+
+        if sample_weights is None:
+            self._sample_weights = None
+            # self._sample_weights = torch.tensor([1 / self._size] * self._size,
+            #                                     dtype=torch.float64,
+            #                                     requires_grad=False)
+        else:
+            if len(sample_weights) != self._size:
+                raise ValueError('len(weights) != datase size')
+            self._sample_weights = torch.tensor(sample_weights,
+                                                requires_grad=False)
+        self._same_samples_over_epochs = same_samples_over_epochs
+        self._sample_then_shuffle = sample_then_shuffle_every_epoch
+        self._cache = None
 
     def __iter__(self):
-        i_s = torch.multinomial(self.weights,
-                                self.size,
-                                replacement=True)
-        return (self.indices[i] for i in i_s)
+        if self._same_samples_over_epochs and self._cache is not None:
+            print('\n\n == WeightedDatasetSampler ==')
+            print(len(self._cache))
+            print(self._cache)
+            return iter(self._cache)
+
+        if self._sample_weights is None:
+            indices = self._dataset_indices
+        else:
+            indices = torch.multinomial(self._sample_weights,
+                                        self._size,
+                                        replacement=True)
+        if self._sample_then_shuffle:
+            indices = indices[torch.randperm(len(indices))]
+
+        datsaet_indices = [self._dataset_indices[idx] for idx in indices]
+
+        if self._same_samples_over_epochs:
+            self._cache = datsaet_indices
+        print('\n\n == WeightedDatasetSampler ==')
+        print(len(datsaet_indices))
+        print(datsaet_indices)
+
+        return iter(datsaet_indices)
 
     def __len__(self):
-        return self.size
+        return self._size
